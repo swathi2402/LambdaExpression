a8e50ceee19aee27f70cf065bb2140449edd12d1
Swathi <swathi.h@perfios.com>
2021-09-13 17:51:45 +0530

    MODIFIED: Using static function to print the result of math operation

--- src/main/java/com/bridgelabz/lambdaexpression/LambdaFunction.java
+++ src/main/java/com/bridgelabz/lambdaexpression/LambdaFunction.java
@@ -2,7 +2,10 @@ package com.bridgelabz.lambdaexpression;
 
 @FunctionalInterface
 interface IMathFunction {
-	int calculate(int x, int y);
+	int calculate(int a, int b);
+	static void printResult(int a, int b, String function, IMathFunction fobj) {
+		System.out.println("Result of "+function+" is "+fobj.calculate(a, b));
+	}
 }
 
 public class LambdaFunction {
@@ -17,5 +20,8 @@ public class LambdaFunction {
 		System.out.println("Subtraction is " + subtract.calculate(6, 3));
 		System.out.println("Division is " + divide.calculate(6, 3));
 
+		IMathFunction.printResult(6, 3, "Addition", add);
+		IMathFunction.printResult(6, 3, "Subtraction", subtract);
+		IMathFunction.printResult(6, 3, "Division", divide);
 	}
 }
