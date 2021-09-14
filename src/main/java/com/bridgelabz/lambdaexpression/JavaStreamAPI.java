7281f6e29df5c1aef4dacc2c6e5774e5d9147acd
Swathi <swathi.h@perfios.com>
2021-09-14 15:29:51 +0530

    MODIFIED: Changed indentation

--- src/main/java/com/bridgelabz/lambdaexpression/JavaStreamAPI.java
+++ src/main/java/com/bridgelabz/lambdaexpression/JavaStreamAPI.java
@@ -18,54 +18,38 @@ public class JavaStreamAPI {
 
 		Function<Integer, Double> toDouble = Integer::doubleValue;
 		Predicate<Integer> isEvenFunction = n -> n > 0 && n % 2 == 0;
-		
+
 		myList.stream().forEach(n -> {
 			System.out.println("Method1: Stream forEach value:" + n);
 		});
 
-		List<Double> doubleList = myList.stream()
-									.map(toDouble)
-									.collect(Collectors.toList());
+		List<Double> doubleList = myList.stream().map(toDouble).collect(Collectors.toList());
 		System.out.println("Printing Double List: " + doubleList);
-		
-		List<Integer> evenList = myList.stream()
-								.filter(isEvenFunction)
-								.collect(Collectors.toList());
+
+		List<Integer> evenList = myList.stream().filter(isEvenFunction).collect(Collectors.toList());
 		System.out.println("Printing Even List: " + evenList);
-		
-		Integer firstEven = myList.stream()
-								.filter(isEvenFunction)
-								.peek(n -> System.out.println("Peek Even Number: " + n))
-								.findFirst()
-								.orElse(null);
+
+		Integer firstEven = myList.stream().filter(isEvenFunction)
+				.peek(n -> System.out.println("Peek Even Number: " + n)).findFirst().orElse(null);
 		System.out.println("First Even Number: " + firstEven);
-		
-		Integer min = myList.stream()
-						.filter(isEvenFunction)
-						.min((n1, n2) -> n1 -n2)
-						.orElse(null);
+
+		Integer min = myList.stream().filter(isEvenFunction).min((n1, n2) -> n1 - n2).orElse(null);
 		System.out.println("Minimum Even Number: " + min);
-		
-		Integer max = myList.stream()
-						.filter(isEvenFunction)
-						.max(Comparator.comparing(Integer::intValue))
-						.orElse(null);
+
+		Integer max = myList.stream().filter(isEvenFunction).max(Comparator.comparing(Integer::intValue)).orElse(null);
 		System.out.println("Maximun Even Number: " + max);
-		
-		Integer sum = myList.stream()
-						.reduce(0, Integer::sum);
+
+		Integer sum = myList.stream().reduce(0, Integer::sum);
 		System.out.println("Sum of intergers: " + sum);
 		long count = myList.stream().count();
-		System.out.println("Average is: " + sum + "/" + count + " = " + sum/count);
-		
+		System.out.println("Average is: " + sum + "/" + count + " = " + sum / count);
+
 		boolean allEven = myList.stream().allMatch(isEvenFunction);
 		boolean anyEven = myList.stream().anyMatch(isEvenFunction);
 		System.out.println("Is all Even? :" + allEven);
 		System.out.println("Is any Even? :" + anyEven);
-		
-		List<Integer> sortedList = myList.stream()
-									.sorted((n1, n2) -> n2.compareTo(n1))
-									.collect(Collectors.toList());
+
+		List<Integer> sortedList = myList.stream().sorted((n1, n2) -> n2.compareTo(n1)).collect(Collectors.toList());
 		System.out.println("Sorted List: " + sortedList);
 	}
 }
