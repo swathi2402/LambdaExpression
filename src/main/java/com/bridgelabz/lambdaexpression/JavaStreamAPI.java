3be36cc394bf6f6858371610278dd0086acc8da4
Swathi <swathi.h@perfios.com>
2021-09-14 14:59:20 +0530

    ADDED: Used filter of stream to get eveninteger in list elements

--- src/main/java/com/bridgelabz/lambdaexpression/JavaStreamAPI.java
+++ src/main/java/com/bridgelabz/lambdaexpression/JavaStreamAPI.java
@@ -3,6 +3,7 @@ package com.bridgelabz.lambdaexpression;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class JavaStreamAPI {
@@ -14,12 +15,21 @@ public class JavaStreamAPI {
 			myList.add(i);
 		}
 
+		Function<Integer, Double> toDouble = Integer::doubleValue;
+		Predicate<Integer> isEvenFunction = n -> n > 0 && n % 2 == 0;
+		
 		myList.stream().forEach(n -> {
 			System.out.println("Method1: Stream forEach value:" + n);
 		});
 
-		Function<Integer, Double> toDouble = Integer::doubleValue;
-		List<Double> doubleList = myList.stream().map(toDouble).collect(Collectors.toList());
+		List<Double> doubleList = myList.stream()
+									.map(toDouble)
+									.collect(Collectors.toList());
 		System.out.println("Printing Double List: " + doubleList);
+		
+		List<Integer> evenList = myList.stream()
+								.filter(isEvenFunction)
+								.collect(Collectors.toList());
+		System.out.println("Printing Even List: " + evenList);
 	}
 }
