085a3134722f2ff9c0d5d8ec276d9cde9d37b630
Swathi <swathi.h@perfios.com>
2021-09-14 14:53:39 +0530

    ADDED: Used map of stream to convert integer to double of list elements

--- src/main/java/com/bridgelabz/lambdaexpression/JavaStreamAPI.java
+++ src/main/java/com/bridgelabz/lambdaexpression/JavaStreamAPI.java
@@ -2,6 +2,8 @@ package com.bridgelabz.lambdaexpression;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class JavaStreamAPI {
 
@@ -11,9 +13,13 @@ public class JavaStreamAPI {
 		for (int i = 0; i < 5; i++) {
 			myList.add(i);
 		}
-		
+
 		myList.stream().forEach(n -> {
 			System.out.println("Method1: Stream forEach value:" + n);
 		});
+
+		Function<Integer, Double> toDouble = Integer::doubleValue;
+		List<Double> doubleList = myList.stream().map(toDouble).collect(Collectors.toList());
+		System.out.println("Printing Double List: " + doubleList);
 	}
 }
